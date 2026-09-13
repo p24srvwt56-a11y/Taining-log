43fada9df624a5e61bb0a3db8ec447128e018e65
p24srvwt56-a11y <p24srvwt56@privaterelay.appleid.com>
2026-09-13 12:25:29 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const C = "training-log-v2";
+const FILES = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
